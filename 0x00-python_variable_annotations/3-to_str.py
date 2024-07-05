b930740df174ea838cf195e4f655a0451e0ee3fb
francotaind <francotaind00@gmail.com>
2024-07-05 08:29:18 +0300

    qstn3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task: to_str."""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)

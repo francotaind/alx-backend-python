a9c6657c1c2e2c020b3b82114ef60bf13210afe7
francotaind <francotaind00@gmail.com>
2024-07-05 08:23:00 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math
+'''Task 2: Floor module
+'''
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given float number.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor of the input number.
+    """
+    return math.floor(n)

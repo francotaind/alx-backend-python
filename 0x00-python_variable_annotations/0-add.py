63aff21a9230db774a2425f9bc4386639b252298
francotaind <francotaind00@gmail.com>
2024-07-05 08:01:24 +0300

    ANNOTATIONS

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
 def add(a: float, b: float) -> float:
-    """Return the sum of two floats."""
+    '''Adds two floating-point numbers.
+    '''
     return a + b

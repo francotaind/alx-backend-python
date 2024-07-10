7b0805839ae20a9f045a6ca9c621ee9543a27e8c
francotaind <francotaind00@gmail.com>
2024-07-10 08:54:12 +0300

    fixing task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous function that waits for a random delay between 0 and
     `max_delay` seconds and then returns it.
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,17 +8,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Execute wait_random n times concurrently
-    Args:
-        n: number of times to call wait_random
-        max_delay: maximum wait time in seconds
-    Returns:
-        list of float numbers
-    """
-    tasks = []
-    for _ in range(n):
-        tasks.append(wait_random(max_delay))
+    """Execute wait_random n times concurrently"""
 
-    delays = []
-    for task in asyncio.as_completed(tasks):
-        delays.append(await task)
+    wait_times = await asyncio.gather(*tuple(map(lambda _:
+        wait_random(max_delay), range(n))))
+    return sorted(wait_times)
